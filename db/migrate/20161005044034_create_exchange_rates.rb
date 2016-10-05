111985b5a8059a3375ccfd1ec1d47940cbbb46f2
ar.shestopal <ar.shestopal@gmail.com>
2016-10-05 08:04:31 +0300

    Changed date to string for flexibility

--- db/migrate/20161005044034_create_exchange_rates.rb
+++ db/migrate/20161005044034_create_exchange_rates.rb
@@ -1,7 +1,7 @@
 class CreateExchangeRates < ActiveRecord::Migration[5.0]
   def change
     create_table :exchange_rates do |t|
-      t.datetime :date
+      t.string :date
       t.float :rate
 
       t.timestamps
